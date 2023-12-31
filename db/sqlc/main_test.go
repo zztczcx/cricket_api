c8ba2668e58d39ce6965142d595d698d14978a56
jozhao <jozhao@seek.com.au>
2023-12-31 18:21:14 +1100

    using transaction rollback for clean test db

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -2,25 +2,50 @@ package db
 
 import (
 	"cricket/config"
+	"database/sql"
+	"errors"
 	"log"
 	"os"
+	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
-var testStore Store
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-        cfg, err := config.Load()
+	cfg, err := config.Load()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	connPool, err := NewDatabase(cfg.Database)
+	ensureUsingTestDB(cfg.Database.DatabaseURL)
+
+	testDB, err = NewDatabase(cfg.Database)
 	if err != nil {
 		panic(err)
 	}
 
-        testStore = NewStore(connPool)
+	os.Exit(m.Run())
+}
 
-        os.Exit(m.Run())
+func TxStore(t *testing.T) Store {
+	tx, err := testDB.Begin()
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Cleanup(func() {
+		err := tx.Rollback()
+		require.NoError(t, err)
+	})
+
+	return New(testDB).WithTx(tx)
+}
+
+func ensureUsingTestDB(url string) {
+	if !strings.Contains(url, "test") {
+		panic(errors.New("not using test database"))
+	}
 }
--- db/sqlc/players_test.go
+++ db/sqlc/players_test.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"math/rand"
+
 	"sort"
 	"strconv"
 	"testing"
@@ -10,7 +11,87 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func createRandomPlayer(t *testing.T) CreatePlayerParams {
+func Test_CreatePlayer(t *testing.T) {
+	t.Parallel()
+	s := TxStore(t)
+
+	createRandomPlayer(t, s)
+}
+
+func Test_GetPlayersByCareerYear(t *testing.T) {
+	t.Parallel()
+	s := TxStore(t)
+
+	p1 := createRandomPlayer(t, s)
+	p2 := createRandomPlayer(t, s)
+
+	arg := GetPlayersByCareerYearParams{
+		CareerYear: ToNullInt64("2010"),
+	}
+
+	players, err := s.GetPlayersByCareerYear(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, players)
+
+	require.Equal(t, p1.Name, players[0].Name)
+	require.Equal(t, p2.Name, players[1].Name)
+
+	arg = GetPlayersByCareerYearParams{
+		CareerYear: ToNullInt64("2040"),
+	}
+
+	players, err = s.GetPlayersByCareerYear(context.Background(), arg)
+	require.NoError(t, err)
+	require.Empty(t, players)
+}
+
+func Test_GetPlayerOfMostRuns(t *testing.T) {
+	t.Parallel()
+	s := TxStore(t)
+
+	p1 := createRandomPlayer(t, s)
+	p2 := createRandomPlayer(t, s)
+	p3 := createRandomPlayer(t, s)
+
+	players := []CreatePlayerParams{p1, p2, p3}
+	sort.SliceStable(players, func(i, j int) bool { return players[i].Runs.Int64 > players[j].Runs.Int64 })
+
+	player, err := s.GetPlayerOfMostRuns(context.Background())
+	require.NoError(t, err)
+	require.NotEmpty(t, player)
+
+	require.Equal(t, player.Name, players[0].Name)
+}
+
+func Test_GetPlayerOfMostRunsByCareerEndYear(t *testing.T) {
+	t.Parallel()
+	s := TxStore(t)
+
+	s.CreatePlayer(context.Background(), CreatePlayerParams{
+		Name:          "player_1",
+		CareerEndYear: ToNullInt64("2011"),
+		Runs:          ToNullInt64("1111")},
+	)
+
+	s.CreatePlayer(context.Background(), CreatePlayerParams{
+		Name:          "player_2",
+		CareerEndYear: ToNullInt64("2011"),
+		Runs:          ToNullInt64("1122")},
+	)
+	s.CreatePlayer(context.Background(), CreatePlayerParams{
+		Name:          "player_3",
+		CareerEndYear: ToNullInt64("2000"),
+		Runs:          ToNullInt64("1111")},
+	)
+
+	player, err := s.GetPlayerOfMostRunsByCareerEndYear(context.Background(), ToNullInt64("2011"))
+	require.NoError(t, err)
+	require.NotEmpty(t, player)
+
+	require.Equal(t, player.Name, "player_2")
+}
+
+func createRandomPlayer(t *testing.T, s Store) CreatePlayerParams {
 	arg := CreatePlayerParams{
 		Name:            "player_" + strconv.Itoa(rand.Intn(1000)),
 		CareerStartYear: ToNullInt64("19" + strconv.Itoa(rand.Intn(90)+10)),
@@ -28,88 +109,12 @@ func createRandomPlayer(t *testing.T) CreatePlayerParams {
 		ScoreZeros:      ToNullInt64(strconv.Itoa(rand.Intn(2000))),
 	}
 
-	player, err := testStore.CreatePlayer(context.Background(), arg)
+	result, err := s.CreatePlayer(context.Background(), arg)
 	require.NoError(t, err)
-	require.NotEmpty(t, player)
+
+	id, err := result.LastInsertId()
+	require.NoError(t, err)
+	require.NotEmpty(t, id)
 
 	return arg
-}
-
-func clearTable(t *testing.T) {
-	testStore.DeleteAllPlayers(context.Background())
-}
-
-func Test_CreatePlayer(t *testing.T) {
-	defer clearTable(t)
-	createRandomPlayer(t)
-}
-
-func Test_GetPlayersByCareerYear(t *testing.T) {
-	defer clearTable(t)
-
-	p1 := createRandomPlayer(t)
-	p2 := createRandomPlayer(t)
-
-	arg := GetPlayersByCareerYearParams{
-		CareerYear: ToNullInt64("2010"),
-	}
-
-	players, err := testStore.GetPlayersByCareerYear(context.Background(), arg)
-	require.NoError(t, err)
-	require.NotEmpty(t, players)
-
-	require.Equal(t, p1.Name, players[0].Name)
-	require.Equal(t, p2.Name, players[1].Name)
-
-	arg = GetPlayersByCareerYearParams{
-		CareerYear: ToNullInt64("2040"),
-	}
-
-	players, err = testStore.GetPlayersByCareerYear(context.Background(), arg)
-	require.NoError(t, err)
-	require.Empty(t, players)
-}
-
-func Test_GetPlayerOfMostRuns(t *testing.T) {
-	defer clearTable(t)
-
-	p1 := createRandomPlayer(t)
-	p2 := createRandomPlayer(t)
-	p3 := createRandomPlayer(t)
-
-	players := []CreatePlayerParams{p1, p2, p3}
-	sort.SliceStable(players, func(i, j int) bool { return players[i].Runs.Int64 > players[j].Runs.Int64 })
-
-	player, err := testStore.GetPlayerOfMostRuns(context.Background())
-	require.NoError(t, err)
-	require.NotEmpty(t, player)
-
-	require.Equal(t, player.Name, players[0].Name)
-}
-
-func Test_GetPlayerOfMostRunsByCareerEndYear(t *testing.T) {
-	defer clearTable(t)
-
-	testStore.CreatePlayer(context.Background(), CreatePlayerParams{
-		Name:          "player_1",
-		CareerEndYear: ToNullInt64("2011"),
-		Runs:          ToNullInt64("1111")},
-	)
-
-	testStore.CreatePlayer(context.Background(), CreatePlayerParams{
-		Name:          "player_2",
-		CareerEndYear: ToNullInt64("2011"),
-		Runs:          ToNullInt64("1122")},
-	)
-	testStore.CreatePlayer(context.Background(), CreatePlayerParams{
-		Name:          "player_3",
-		CareerEndYear: ToNullInt64("2000"),
-		Runs:          ToNullInt64("1111")},
-	)
-
-	player, err := testStore.GetPlayerOfMostRunsByCareerEndYear(context.Background(), ToNullInt64("2011"))
-	require.NoError(t, err)
-	require.NotEmpty(t, player)
-
-	require.Equal(t, player.Name, "player_2")
 }
